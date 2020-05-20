9445eaec7472df6edacaf6ce609b263c7f2c68fb
BenBowers <ben.bowers720@outlook.com>
2020-05-20 13:08:22 +0800

    Initial Commit

---
+++ OOSE_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOSE_Assignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
