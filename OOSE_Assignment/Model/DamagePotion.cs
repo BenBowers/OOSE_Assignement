ee6d062b04dc65ffcb82a5b418b2493a7122bb73
BenBowers <ben.bowers720@outlook.com>
2020-05-21 10:44:02 +0800

    Added potions

---
+++ OOSE_Assignment/Model/DamagePotion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace OOSE_Assignment.Model
+{
+    public class DamagePotion : Potion
+    {
+        public DamagePotion(string name, int cost, int minEffect, int maxEffect)
+            : base(name, cost, minEffect, maxEffect) { }
+
+        public override void Use(Character character) =>
+            character.Damage(GetEffect());
+    }
+}
---
+++ OOSE_Assignment/Model/HealingPotion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace OOSE_Assignment.Model
+{
+    public class HealingPotion : Potion
+    {
+        public HealingPotion(string name, int cost, int minEffect, int maxEffect)
+            : base(name, cost, minEffect, maxEffect) { }
+
+        public override void Use(Character character)
+            => character.Heal(GetEffect());
+    }
+}
