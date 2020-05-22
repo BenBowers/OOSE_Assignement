a3b97d38b93a23b1d91aa6c740ddc858867b895c
BenBowers <ben.bowers720@outlook.com>
2020-05-22 13:34:04 +0800

    Moved menu item to it's own file

--- OOSE_Assignment/View/Menu.cs
+++ OOSE_Assignment/View/Menu.cs
@@ -3,25 +3,6 @@ using System.Collections.Generic;
 
 namespace OOSE_Assignment.View
 {
-
-    public delegate void MenuAction();
-    public class MenuItem
-    {
-        public MenuItem(string name,  MenuAction action)
-        {
-            Name = name;
-            Action = action;
-        }
-
-        public string Name { get; set; }
-        public event MenuAction Action;
-
-        public void Run()
-        {
-            Action();
-        }
-    }
-
     public abstract class Menu
     {
 
---
+++ OOSE_Assignment/View/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿namespace OOSE_Assignment.View
+{
+    public delegate void MenuAction();
+    public class MenuItem
+    {
+        public MenuItem(string name, MenuAction action)
+        {
+            Name = name;
+            Action = action;
+        }
+
+        public string Name { get; set; }
+        public event MenuAction Action;
+
+        public void Run()
+        {
+            Action();
+        }
+    }
+}
