101cdf267c585786335fb67655afd7a897db1597
BenBowers <ben.bowers720@outlook.com>
2020-05-25 17:24:19 +0800

    Added a name prompt

---
+++ OOSE_Assignment/Controller/NamePrompt.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using OOSE_Assignment.View;
+namespace OOSE_Assignment.Controller
+{
+    public class NamePrompt : Prompt
+    {
+        public const string USER_PROMPT = "Please enter player name";
+        public NamePrompt() : base(USER_PROMPT) { }
+        public string GetName()
+        {
+            return Run();
+        }
+    }
+}
---
+++ OOSE_Assignment/View/Prompt.cs
@@ -0,0 +1,25 @@
+﻿//
+//  Prompt.cs
+//
+//  Author:
+//       benjaminbowers <>
+//
+//
+using System;
+namespace OOSE_Assignment.View
+{
+    public class Prompt
+    {
+        public string Message;
+        public Prompt(string message)
+        {
+            Message = message;
+        }
+
+        protected string Run()
+        {
+            System.Console.Write(Message + ": ");
+            return System.Console.ReadLine();
+        }
+    }
+}
