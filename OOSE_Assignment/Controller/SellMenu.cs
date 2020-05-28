ef6e3a4b7ecec04d2dc5c26868fcf6ddfeba6890
BenBowers <ben.bowers720@outlook.com>
2020-05-28 09:36:15 +0800

    Added the option to sell items

---
+++ OOSE_Assignment/Controller/SellMenu.cs
@@ -0,0 +1,76 @@
+﻿//
+//  SellMenu.cs
+//
+//  Author:
+//       benjaminbowers <>
+//
+//
+using System;
+using System.Collections.Generic;
+using OOSE_Assignment.View;
+using OOSE_Assignment.Model;
+namespace OOSE_Assignment.Controller
+{
+    public class SellMenu : MethodMenu
+    {
+        public const string NAME = "Shop";
+        public const string STATUS_BAR = "Current Gold: {0}, Inventory {1}/{2}";
+        private Shop shop;
+        private Player player;
+
+        private string Status => String.Format(STATUS_BAR,
+                    player.Gold,
+                    player.Inventory.Capacity,
+                    Inventory.INVENTORY_SIZE);
+
+        public SellMenu(Player player, Shop shop)
+        {
+            menuName = NAME;
+            options = new List<MenuItem>()
+            {
+                new MenuItem("Weapons", WeaponOption),
+                new MenuItem("Armour", ArmourOption),
+                new MenuItem("Potions", PotionOption)
+            };
+
+            exit = ExitOption;
+            this.shop = shop;
+            this.player = player;
+        }
+
+        private void WeaponOption()
+        {
+            ObjectMenu<Weapon> menu = new ObjectMenu<Weapon>(shop.Weapons, Status);
+            Weapon weapon = menu.Run();
+            player.Inventory.RemoveItem(weapon);
+            player.AddGold(weapon.Cost);
+            this.Run();
+        }
+
+        private void ArmourOption()
+        {
+            ObjectMenu<Armour> menu = new ObjectMenu<Armour>(shop.Armours, Status);
+            Armour armour = menu.Run();
+            player.Inventory.RemoveItem(armour);
+            player.AddGold(armour.Cost);
+            this.Run();
+        }
+
+        private void PotionOption()
+        {
+            ObjectMenu<Potion> menu = new ObjectMenu<Potion>(shop.Potions, Status);
+            Potion potion = menu.Run();
+            player.Inventory.RemoveItem(potion);
+            player.AddGold(potion.Cost);
+            this.Run();
+        }
+
+        private void ExitOption()
+        {
+
+        }
+
+
+    }
+}
+
--- OOSE_Assignment/Controller/ShopMenu.cs
+++ OOSE_Assignment/Controller/ShopMenu.cs
@@ -1,127 +1,49 @@
-﻿using System;
+﻿//
+//  ShopMenu.cs
+//
+//  Author:
+//       benjaminbowers <>
+//
+//
+using System;
 using System.Collections.Generic;
-using OOSE_Assignment.View;
 using OOSE_Assignment.Model;
+using OOSE_Assignment.View;
+
 namespace OOSE_Assignment.Controller
 {
     public class ShopMenu : MethodMenu
     {
-        public const string NAME = "Shop";
-        public const string STATUS_BAR = "Current Gold: {0}, Inventory {1}/{2}";
-        private const string INSUFFCIENT_FUNDS_PROMPT = "You do not have enough gold to purchase that!!";
-        private Shop shop;
         private Player player;
-
-        private string Status => String.Format(STATUS_BAR,
-                    player.Gold,
-                    player.Inventory.Capacity,
-                    Inventory.INVENTORY_SIZE);
-
+        private Shop shop;
         public ShopMenu(Player player, Shop shop)
         {
-            menuName = NAME;
-            options = new List<MenuItem>()
+            options = new List<MenuItem>
             {
-                new MenuItem("Weapons", WeaponOption),
-                new MenuItem("Armour", ArmourOption),
-                new MenuItem("Potions", PotionOption),
-                new MenuItem("Enchant", EnchantmentOption)
+                new MenuItem("Buy", Buy),
+                new MenuItem("Sell", Sell)
             };
+            exit = Exit;
 
-            exit = ExitOption;
-            this.shop = shop;
             this.player = player;
+            this.shop = shop;
         }
 
-        private void WeaponOption()
+        private void Buy()
         {
-            ObjectMenu<Weapon> menu = new ObjectMenu<Weapon>(shop.Weapons, Status);
-            Weapon weapon = menu.Run();
-            if (weapon != null)
-            {
-                if (weapon.Cost < player.Gold)
-                {
-                    player.RemoveGold(weapon.Cost);
-                    player.Inventory.AddItem(weapon);
-                }
-                else
-                {
-                    Console.WriteLine(INSUFFCIENT_FUNDS_PROMPT);
-                }
-            }
+            new BuyMenu(player, shop).Run();
             this.Run();
         }
 
-        private void ArmourOption()
+        private void Sell()
         {
-            ObjectMenu<Armour> menu = new ObjectMenu<Armour>(shop.Armours, Status);
-            Armour armour = menu.Run();
-            if (armour != null)
-            {
-                if (armour.Cost < player.Gold)
-                {
-                    player.RemoveGold(armour.Cost);
-                    player.Inventory.AddItem(armour);
-                }
-                else
-                {
-                    Console.WriteLine(INSUFFCIENT_FUNDS_PROMPT);
-                }
-            }
+            new SellMenu(player, shop).Run();
             this.Run();
         }
 
-        private void PotionOption()
-        {
-            ObjectMenu<Potion> menu = new ObjectMenu<Potion>(shop.Potions, Status);
-            Potion potion = menu.Run();
-            if (potion != null)
-            {
-                if (potion.Cost < player.Gold)
-                {
-                    player.RemoveGold(potion.Cost);
-                    player.Inventory.AddItem(potion);
-                }
-                else
-                {
-                    Console.WriteLine(INSUFFCIENT_FUNDS_PROMPT);
-                }
-            }
-            this.Run();
-        }
-
-
-        private void EnchantmentOption()
-        {
-            ObjectMenu<WeaponEnchantment> menu =
-                new ObjectMenu<WeaponEnchantment>(shop.Enchantments, "Enchant weapon");
-            WeaponEnchantment enchantment = menu.Run();
-            if (enchantment != null)
-            {
-                if (enchantment.Cost < player.Gold)
-                {
-                    ObjectMenu<Weapon> weaponMenu = new ObjectMenu<Weapon>(player.Inventory.Weapons, "Select weapon to enchant");
-                    Weapon weapon = weaponMenu.Run();
-                    if (weapon != null)
-                    {
-                        player.RemoveGold(enchantment.Cost);
-                        weapon.AddEnchantment(enchantment);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(INSUFFCIENT_FUNDS_PROMPT);
-                }
-            }
-
-            this.Run();
-        }
-
-        private void ExitOption()
+        private void Exit()
         {
 
         }
-
-
     }
 }
